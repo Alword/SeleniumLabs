72c4f320f546979d8c194d5bad2765e7faa097b7
Alword <anzer987@yandex.ru>
2020-02-26 23:52:16 +0300

    feature drive close list

--- lab1.py
+++ lab1.py
@@ -1,36 +1,38 @@
 # Импортируем необходимые библиотеки
 from selenium import webdriver
 from random import shuffle
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
+
 # Зададим драйвер нашего браузера
 
+def close_tab(windows):
+    driver = windows[:-1]
+    driver.quit()
+    windows.remove(driver)
 
-def open_tab(link):
-    driver.execute_script(
-        'window.open("{0}","_blank");'.format(link.replace('\n', '')))
 
-
-opts = Options()
-opts.set_preference("browser.link.open_newwindow.restriction", 0)
-opts.set_preference("browser.link.open_newwindow", 3)
-
-driver = webdriver.Firefox(firefox_options=opts,
-                           executable_path='./geckodriver.exe',
-                           firefox_binary='C:/Users/aasle/AppData/Local/Mozilla Firefox/firefox.exe')
+def open_tab(link, windows):
+    if 'http' in link:
+        driver = webdriver.Firefox(executable_path='./geckodriver.exe',
+                                   firefox_binary='C:/Users/aasle/AppData/Local/Mozilla Firefox/firefox.exe')
+        driver.get(link.replace('\n', ''))
+        windows.append(driver)
 
 # Метод driver.get(url) перенаправляет к странице с адресом url.
 # WebDriver будет ждать пока страница не загрузится полностью,
 # прежде чем передать контроль вашему тесту или скрипту.
+
 
 openstream = open("randomsite.txt", encoding="UTF-8-sig")
 sites = openstream.readlines()
 
 shuffle(sites)
 
+windows = []
+
 for link in sites:
-    open_tab(link)
+    open_tab(link, windows)
+
+while len(windows) > 0:
+    close_tab(windows)
 
 open("tempsites.txt", 'w+', encoding="UTF-8-sig",).writelines(sites)
-
-driver.close()
